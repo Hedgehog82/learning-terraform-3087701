bfa4314673c275a6209b5b818220e63f32b5f58f
Hedgehog82 <svsfeedback@gmail.com>
2025-05-19 12:54:39 +0400

    target_type is set IP

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ module "blog_alb" {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
-      target_type      = "instance"
+      target_type      = "ip"
     }
   }
 
